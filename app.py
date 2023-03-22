16f5108d1d711fa6be286b8521ac037fdee8ef80
Victor Martins <104099151+vic-martins@users.noreply.github.com>
2023-03-22 16:17:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -227,6 +227,7 @@ def page2():
     dist = 'dist_' + cen1
     upper = 'yhat_upper_' + cen1
     lower = 'yhat_lower_' + cen1
+    st.table(df23['dist_Pessimista'])
     st.write(dist, upper, lower)
     csv = df23[['data', dist, upper, lower]][:]
     csv = convert_df(csv)
