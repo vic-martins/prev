e911c0f1df22205805fff51ba32f17330de497c6
Victor Martins <104099151+vic-martins@users.noreply.github.com>
2023-03-22 16:13:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -227,7 +227,8 @@ def page2():
     dist = 'dist_' + cen1
     upper = 'yhat_upper_' + cen1
     lower = 'yhat_lower_' + cen1
-    csv = convert_df(df23[['data', dist, upper, lower]])
+    csv = df23[['data', dist, upper, lower]][:]
+    csv = convert_df(csv)
         
     st.download_button(
       "Baixar dados do cenário 1",
